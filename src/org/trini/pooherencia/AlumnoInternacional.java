204de6bcde98c327f64b736969bf09b63d282951
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-17 16:18:48 -0300

    creacion clase AlumnoInternacional

---
+++ src/org/trini/pooherencia/AlumnoInternacional.java
@@ -0,0 +1,22 @@
+package org.trini.pooherencia;
+
+public class AlumnoInternacional extends Alumno{
+    private String pais;
+    private String notaIdiomas;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getNotaIdiomas() {
+        return notaIdiomas;
+    }
+
+    public void setNotaIdiomas(String notaIdiomas) {
+        this.notaIdiomas = notaIdiomas;
+    }
+}
